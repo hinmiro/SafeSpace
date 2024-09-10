3918e2e521f4ee8bb4eca83fc638c3796057ec6f
sarapappila <sara.pappila@metropolia.fi>
2024-09-10 20:59:51 +0300

    Lisätään uusi fxml (logOut), tehdään sille pohja ja css

---
+++ src/main/java/controller/LogOutController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import view.View;
+
+public class LogOutController {
+
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private Button cancelButton;
+
+    private Stage dialogStage;
+    private View mainView;
+
+    @FXML
+    private void initialize() {
+        logOutButton.setOnAction(event -> {
+            try {
+                handleLogOut();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        cancelButton.setOnAction(event -> handleCancel());
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setMainView(View mainView) {
+        this.mainView = mainView;
+    }
+
+
+    // todo
+    private void handleLogOut() throws Exception {
+        mainView.showLogin();
+        if (dialogStage != null) {
+            dialogStage.close();
+        }
+    }
+
+    private void handleCancel() {
+        if (dialogStage != null) {
+            dialogStage.close();
+        }
+    }
+}
--- src/main/java/controller/ProfileController.java
+++ src/main/java/controller/ProfileController.java
@@ -8,19 +8,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import javafx.scene.shape.Circle;
-import javafx.scene.control.ContextMenu;
+import view.View;
 
 public class ProfileController {
 
@@ -52,6 +52,7 @@ public class ProfileController {
         MenuItem editProfileItem = new MenuItem("Edit Profile");
         MenuItem editInfoItem = new MenuItem("Update Info");
         MenuItem logOutItem = new MenuItem("Log Out");
+        logOutItem.setOnAction(event -> showCustomDialog());
 
         settingsContextMenu.getItems().addAll(editProfileItem, editInfoItem, logOutItem);
         settingsProfileID.setOnMouseClicked(event -> showContextMenu(event));
@@ -59,6 +60,24 @@ public class ProfileController {
 
     private void showContextMenu(MouseEvent event) {
         settingsContextMenu.show(settingsProfileID, Side.BOTTOM, 0, 0);
+    }
+
+    private void showCustomDialog() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/logout.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Log Out");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            LogOutController logOutController = fxmlLoader.getController();
+            logOutController.setDialogStage(stage);
+            logOutController.setMainView(new View());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void makeCircle(ImageView imageView) {
