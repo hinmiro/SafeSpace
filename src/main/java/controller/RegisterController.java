023e0a5e831908e4ee41fd7c238c6e45d948de0d
sarapappila <sara.pappila@metropolia.fi>
2024-10-28 16:46:07 +0200

    pois

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -51,18 +51,6 @@ public class RegisterController {
 
     @FXML
     public void initialize() {
-        languageBox.getItems().setAll(
-                Language.EN.getDisplayName(),
-                Language.FI.getDisplayName(),
-                Language.JP.getDisplayName(),
-                Language.RU.getDisplayName()
-        );
-
-        Language currentLanguage = SessionManager.getInstance().getSelectedLanguage();
-        languageBox.setValue(currentLanguage.getDisplayName());
-
-        languageBox.setOnAction(event -> changeLanguage());
-
         registerButton.setOnAction(event -> handleRegister());
         backButton.setOnAction(event -> backLogin());
         usernameField.setOnKeyPressed(event -> handleEnterKey(event));
@@ -86,23 +74,6 @@ public class RegisterController {
         alreadyLabel.setText(labels.getString("alreadyAccount"));
         registerButton.setText(buttons.getString("registerButton"));
         backButton.setText(buttons.getString("backToLogin"));
-    }
-
-    @FXML
-    private void changeLanguage() {
-        String selectedLanguage = languageBox.getValue();
-
-        if (selectedLanguage.equals(Language.FI.getDisplayName())) {
-            SessionManager.getInstance().setLanguage(Language.FI);
-        } else if (selectedLanguage.equals(Language.JP.getDisplayName())) {
-            SessionManager.getInstance().setLanguage(Language.JP);
-        } else if (selectedLanguage.equals(Language.RU.getDisplayName())) {
-            SessionManager.getInstance().setLanguage(Language.RU);
-        } else {
-            SessionManager.getInstance().setLanguage(Language.EN);
-        }
-
-        updateLanguage();
     }
 
     private void updateLanguage() {
